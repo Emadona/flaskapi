f61eaed28ff4c9a52d4e576ba2087ec3caa72fe7
Emadona <kh.emad10@gmail.com>
2021-02-21 06:51:40 +0300

    add my api

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask , jsonify , request
+import json
+
+
+app = Flask(__name__)
+
+response = ''
+
+@app.route('/name' , method = ['GET' , 'POST'])
+def nameroute():
+    global response
+
+    if (request.method == 'POST'):
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        name = request_data['name']
+        response = f'Hi {name} this is Python API'
+        return ''
+    else:
+        return jsonify({'name' : response})
+
+
+if (__name__) == ('__main__'):
+    app.run()
